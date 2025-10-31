9c45b2917272806a613137cf7217a533b526e402
richard <richard-hatslogic>
2025-10-31 10:54:19 +0530

    initial commit

---
+++ src/WGSoftChildTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace WGSoftChildTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class WGSoftChildTheme extends Plugin implements ThemeInterface
+{
+}
